1994298f2ba162e8a8f90078a16f9adc33d3e0c0
MFaad1 <faadsardar123@gmail.com>
2025-05-26 16:28:18 +0500

    fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,6 @@ export async function middleware(req: NextRequest) {
     const { pathname, origin } = req.nextUrl;
     const fullUrl = req.url;
            let redirectedProduct = await fetchRedirectUrlById(pathname.replace(/^\/+|\/+$/g, ''))
-console.log(redirectedProduct, "redirectedProduct")
     if (!fullUrl.endsWith('/')) {
         return NextResponse.redirect(
             new URL(`${req.nextUrl.pathname}/`, req.nextUrl), 301
