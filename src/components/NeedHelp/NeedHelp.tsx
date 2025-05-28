74d95b3952e8e19bddc6a3a5f4c2fa2b3a7bb71e
GHULAM-MUSTAFA101 <gmchaudhary444@gmail.com>
2025-05-28 12:59:03 +0500

    thumbnail fixes and banner images changes

--- src/components/NeedHelp/NeedHelp.tsx
+++ src/components/NeedHelp/NeedHelp.tsx
@@ -15,7 +15,7 @@ const NeedHelp = () => {
                <Link href={`https://wa.me/${WhatsAppInfo.number.replaceAll(' ', '')}`} target='_blank' aria-label="Chat on WhatsApp" className='text-18 xs:text-22 font-medium font-roboto bg-white w-48 xs:w-56 h-14 rounded-md flex gap-2 text-black justify-center items-center' ><Image width={100} height={100} className='size-6 xs:size-7' src='/assets/images/whatsapp.png' alt='whatsapp'/> WhatsApp Us</Link>
             </div>
             <div className='sm:pt-5'>
-              <Image src='/assets/images/callus/needhelp1.png' fill className='!relative object-cover w-full h-auto' alt='need help image' />
+              <Image src='/assets/images/callus/needhelp1.webp' fill className='!relative object-cover w-full h-auto' alt='need help image' />
             </div>
          </div>
       </Container>
