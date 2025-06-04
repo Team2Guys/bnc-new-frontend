383e410b71da71b3026cfe50baccec98077ce4b1
Wajid-Fareed <wajidfareed103@gmail.com>
2025-06-04 14:31:20 +0500

    fixes

--- src/components/BestSellerSlider/SellerCard.tsx
+++ src/components/BestSellerSlider/SellerCard.tsx
@@ -47,16 +47,12 @@ const SellerSlider = ({ products }: { products: IProduct[] }) => {
     dots: false,
     infinite: true,
     speed: 600,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     responsive: [
-      {
-        breakpoint: 1500,
-        settings: { slidesToShow: 4 },
-      },
       {
         breakpoint: 1024,
         settings: { slidesToShow: 3 },
